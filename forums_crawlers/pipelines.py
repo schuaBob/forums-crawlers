3779ec2980fa7cf13a628cfb968f82053b0dad9b
Bob Hua <schua1013@gmail.com>
2023-07-18 08:23:33 -0400

    Add pipelines DownloadPubMedPDF, ParsePDFFromFiles, EncodeTexts, MilvusStore

--- forums_crawlers/pipelines.py
+++ forums_crawlers/pipelines.py
@@ -6,28 +6,138 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-import pymongo
 from urllib.parse import quote_plus
-import os
 from dotenv import load_dotenv
-import logging
+from scrapy.exceptions import DropItem
+from pypdf import PdfReader
+import pymongo
+import os
+from pymilvus import connections, Collection, utility
+from scrapy.pipelines.files import FilesPipeline
+import scrapy
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
+from time import sleep
 
 load_dotenv()
 
 
-class ItemCheckPipeline:
-    def process_item(self, item, spider):
-        item = ItemAdapter(item)
-        length = [
-            len(item.get("datetimes")),
-            len(item.get("user_ids")),
-            len(item.get("user_post_count")),
-            len(item.get("bodys")),
-            len(item.get("replies")),
-        ]
-        if length.count(length[0]) != len(length):
-            spider.log(f"{item.get('url')} - {length}", logging.INFO)
+class DownloadPubMedPDF(FilesPipeline):
+    def file_path(self, request, response=None, info=None, *, item=None):
+        path = os.path.join("pdf", "pubmed", f"{item.get('pmid')}.pdf")
+        return path
 
+    def get_media_requests(self, item, info):
+        adapter = ItemAdapter(item)
+        for file_url in adapter["file_urls"]:
+            if file_url:
+                yield scrapy.Request(file_url)
+
+
+class ParsePDFFromFiles:
+    def __init__(self, filedir):
+        self._filedir = filedir
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.get("FILES_STORE"))
+
+    def process_item(self, item, spider):
+        information = ItemAdapter(item)
+        if not information["files"]:
+            raise DropItem(f"No pmc pdf for pmid:{information.get('pmid')}")
+
+        fullfilename = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            self._filedir,
+            information["files"][0]["path"],
+        )
+        reader = PdfReader(fullfilename)
+        paragraphs, current_paragraph = list(), list()
+        for page in reader.pages:
+            text = page.extract_text()
+            for line in text.splitlines():
+                l = line.strip()
+                if not l and current_paragraph:
+                    paragraphs.append(" ".join(current_paragraph))
+                    current_paragraph.clear()
+                elif l.endswith((".", "?", "!")):
+                    current_paragraph.append(l)
+                    paragraphs.append(" ".join(current_paragraph))
+                    current_paragraph.clear()
+                else:
+                    current_paragraph.append(l)
+        if current_paragraph:
+            paragraphs.append(" ".join(current_paragraph))
+        information["paragraphs"] = paragraphs
+        return item
+
+
+class EncodeTexts:
+    def open_spider(self, spider):
+        self.model = SentenceTransformer(spider.transformer_name)
+
+    def process_item(self, item, spider):
+        information = ItemAdapter(item)
+        information["embeddings"] = self.model.encode(
+            information["paragraphs"], normalize_embeddings=True
+        )
+        return item
+
+
+class MilvusStore:
+    def open_spider(self, spider):
+        connections.connect(
+            alias=os.environ.get("MILVUS_ALIAS"),
+            host=os.environ.get("MILVUS_HOST"),
+            port=os.environ.get("MILVUS_PORT"),
+        )
+        if utility.has_collection(spider.collection_name):
+            Collection(spider.collection_name).drop()
+        self.collection = Collection(spider.collection_name, spider.schema())
+
+    def close_spider(self, spider):
+        print(f"Total entities: {self.collection.num_entities}")
+        if self.collection.num_entities > 1024:
+            self.collection.create_index(
+                spider.schema.index_field, spider.schema.index_params
+            )
+            total = utility.index_building_progress(spider.collection_name)[
+                "total_rows"
+            ]
+            indexed = 0
+            with tqdm(total=total, desc="Building Index") as pbar:
+                while indexed < total:
+                    sleep(0.25)
+                    temp = utility.index_building_progress(spider.collection_name)[
+                        "indexed_rows"
+                    ]
+                    n = temp - indexed
+                    indexed = temp
+                    pbar.update(n)
+        utility.wait_for_index_building_complete(spider.collection_name)
+        connections.disconnect(self._alias)
+
+    def process_item(self, item, spider):
+        info = ItemAdapter(item)
+        self.collection.insert(
+            [
+                [
+                    {
+                        "url": info["file_urls"][0],
+                        "title": info["title"],
+                        "authors": info["authors"],
+                        "year": info["year"],
+                        "paragraph": i + 1,
+                    }
+                    for i in range(len(info["paragraphs"]))
+                ],
+                [paragraph for paragraph in info["paragraphs"]],
+                [embedding for embedding in info["embeddings"]],
+            ]
+        )
+        self.collection.flush()
+        return item
 
 
 class MongoPipeline:
@@ -55,7 +165,7 @@ class MongoPipeline:
             self.collection = self.client[self.mongo_db][self.collection_name]
             self.client.admin.command("ping")
         except Exception as e:
-            spider.log(e, logging.ERROR)
+            spider.logger.error(e)
 
     def close_spider(self, spider):
         self.client.close()
