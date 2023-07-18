7142a9cad7159b34e8f2454bd95dfb1182ff9a3c
Bob Hua <schua1013@gmail.com>
2023-07-18 08:23:54 -0400

    add processor PMIDtoPDFUrl

--- forums_crawlers/processors.py
+++ forums_crawlers/processors.py
@@ -1,3 +1,8 @@
+from typing import Any
+from Bio import Entrez
+import os
+
+
 def to_int(s: str) -> int:
     return int(s.replace(",", ""))
 
@@ -7,3 +12,19 @@ def is_reply(comments: list) -> list:
     if len(comments) > 0:
         res.extend([True for _ in comments])
     return res
+
+
+class PMIDtoPDFUrl:
+    def __init__(self):
+        Entrez.email = os.environ.get("ENTREZ_EMAIL")
+        Entrez.api_key = os.environ.get("ENTREZ_API_KEY")
+
+    def __call__(self, pmid) -> str:
+        url = ""
+        handle = Entrez.elink(id=pmid, cmd="prlinks")
+        for obj in Entrez.read(handle)[0]["IdUrlList"]["IdUrlSet"][0]["ObjUrl"]:
+            if obj["Provider"]["NameAbbr"] == "PMC":
+                url = obj["Url"] + "pdf"
+                break
+        handle.close()
+        return [url]
